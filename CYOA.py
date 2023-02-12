f7ead1fbd59012b52d879e8e333c5ec5fd87f196
CWillR <connerreedwilliams@gmail.com>
2023-02-11 20:27:54 -0600

    Update CYOA.py

--- CYOA.py
+++ CYOA.py
@@ -1,10 +1,38 @@
 import os
+import random
 
-print("Welcome to our Choose Your Own Adventure Game\nIn our game you'll have various options (life choices) that change the outcome of your adventure.\n\n")
-user_input = input("Press '1' and press enter to begin\n")
-if user_input == "1":
-    os.system('cls')
-    print("Prelude:")
-elif user_input != "1":
-    print("you gotta type 1 and only 1 to begin bro")
+class Player:
+    #Player Stats
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+        self.money = 1000.0
+        self.BAC = 0.0
+        self.fun = 0
+    
+    
+def inventory(self):
+    print("Name: ",self.name,"\nWeight: ",self.weight,"\nMoney: $",self.money,"\nBlood Alcohol Content: ",self.BAC)
+        
+
+#Introduction to the initial game laying out the typical usage of the game to the user
+print("Welcome to our Choose Your Own Vacation Game\nIn our game you'll have the option to choose various choices that'll affect the outcome of your adventure.\n\n")
+choice = input("1) Press '1' and press enter to begin\n")
+while choice != "1":
+    print("you gotta type 1 and only 1 to begin bro\n")
+    choice = input("Press '1' and press enter to begin\n")
 os.system('cls')
+
+#Customizing your character and updating the player class with user inputs/random inputs
+print("Character Creator\n")
+name = input("Enter a name: ")
+#Choice for auto-weight or entering your own weight
+choice = input("\nWould you like to enter a weight of your choice or have us choose for you? (random number 100-300)\n1) I'll Enter a custom weight.\n2) Choose for me.\n")
+if(choice == "1"):
+    weight = int(input("Enter your custom weight: "))
+elif(choice == "2"):
+    weight = random.randint(100,300)
+character = Player(name, weight) #Saves the custom character information
+
+#os.system('cls')
+#test
