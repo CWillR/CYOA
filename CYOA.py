61f4c3ea4b2399ef15364d74121f7c8c93a3c6c1
CWillR <connerreedwilliams@gmail.com>
2023-02-11 19:57:00 -0600

    Initial commit

---
+++ CYOA.py
@@ -0,0 +1,10 @@
+import os
+
+print("Welcome to our Choose Your Own Adventure Game\nIn our game you'll have various options (life choices) that change the outcome of your adventure.\n\n")
+user_input = input("Press '1' and press enter to begin\n")
+if user_input == "1":
+    os.system('cls')
+    print("Prelude:")
+elif user_input != "1":
+    print("you gotta type 1 and only 1 to begin bro")
+os.system('cls')
---
+++ README.md
@@ -0,0 +1,2 @@
+# CYOA
+ Choose Your Own Adventure minigame in python for the Auburn Hackathon 2023
